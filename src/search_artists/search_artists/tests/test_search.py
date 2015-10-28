e1d38777392715eca8632efdc514a0ed3bbfff4f
Otávio Augusto Soares <otaviokk@gmail.com>
2015-10-27 23:11:03 -0200

    creating Search

--- src/search_artists/search_artists/tests/test_search.py
+++ src/search_artists/search_artists/tests/test_search.py
@@ -2,4 +2,8 @@ import unittest
 
 
 class SearchTests(unittest.TestCase):
-    pass
+
+    def test_create_a_new_instance_of_Search(self):
+        from search_artists.search import Search
+        search = Search()
+        self.assertIsInstance(search, Search)
