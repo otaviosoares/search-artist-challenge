e870ef8f24571ba0848a4cf45b635b7175f3d250
Otávio Augusto Soares <otaviokk@gmail.com>
2015-10-27 23:12:56 -0200

    first search implementation

--- src/search_artists/search_artists/search.py
+++ src/search_artists/search_artists/search.py
@@ -4,3 +4,9 @@ class Search():
             raise TypeError('data should be of type list')
 
         self.__data = data
+
+    def search(self, minVal, maxVal):
+        def filter_function(x):
+            return x['age'] >= minVal and x['age'] <= maxVal
+
+        return filter(filter_function, self.__data)
--- src/search_artists/search_artists/tests/test_search.py
+++ src/search_artists/search_artists/tests/test_search.py
@@ -1,6 +1,11 @@
 import unittest
 
 
+fake_data = [
+    {'age': 2}, {'age': 5}, {'age': 9}, {'age': 10},
+    {'age': 15}, {'age': 16}, {'age': 20}]
+
+
 class SearchTests(unittest.TestCase):
 
     def test_create_a_new_instance_of_Search(self):
@@ -14,3 +19,9 @@ class SearchTests(unittest.TestCase):
             search = Search('some string')
         exception = cm.exception
         self.assertEquals(exception.message, 'data should be of type list')
+
+    def test_filter_the_data_based_on_provided_parameters(self):
+        from search_artists.search import Search
+        search = Search(fake_data)
+        result = search.search(10, 15)
+        self.assertEquals(len(result), 2)
