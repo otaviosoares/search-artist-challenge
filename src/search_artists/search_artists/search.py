56e53d2919872f36e741751c2f014b7020bd54d2
Otávio Augusto Soares <otaviokk@gmail.com>
2015-10-27 23:11:54 -0200

    validate Search constructor parameter

--- src/search_artists/search_artists/search.py
+++ src/search_artists/search_artists/search.py
@@ -1,2 +1,6 @@
 class Search():
-    pass
+    def __init__(self, data):
+        if not isinstance(data, list):
+            raise TypeError('data should be of type list')
+
+        self.__data = data
--- src/search_artists/search_artists/tests/test_search.py
+++ src/search_artists/search_artists/tests/test_search.py
@@ -5,5 +5,12 @@ class SearchTests(unittest.TestCase):
 
     def test_create_a_new_instance_of_Search(self):
         from search_artists.search import Search
-        search = Search()
+        search = Search([])
         self.assertIsInstance(search, Search)
+
+    def test_should_fail_if_data_provided_is_not_type_list(self):
+        from search_artists.search import Search
+        with self.assertRaises(TypeError) as cm:
+            search = Search('some string')
+        exception = cm.exception
+        self.assertEquals(exception.message, 'data should be of type list')
