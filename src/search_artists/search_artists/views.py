1e30a230494048ee16af165d7117afe3dcb82a9d
Otávio Augusto Soares <otaviokk@gmail.com>
2015-10-28 11:57:34 -0200

    add pyramid view

--- src/search_artists/search_artists/views.py
+++ src/search_artists/search_artists/views.py
@@ -1,13 +1,15 @@
-from pyramid.response import Response
+from pyramid.view import view_config
+from searcher import ArtistSearcher
 
 
+@view_config(route_name='home', renderer='templates/home.pt')
+def my_view(request):
+    return {'minAge': 5, 'maxAge': 50}
+
+
+@view_config(renderer='json')
 def search(request):
-    url = request.url
     mixAge = int(request.params.get('min', 0))
     maxAge = int(request.params.get('max', 100))
 
-    body = 'URL %s with name: %d' % (url, maxAge)
-    return Response(
-        content_type="text/json",
-        body=body
-    )
+    return {}
